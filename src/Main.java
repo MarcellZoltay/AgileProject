6125711daab964670166ea31395b74b7d91458f7
Zoltan Bogaromi <bogaromi@outlook.hu>
2017-11-15 00:00:01 +0100

    Dátumos waringok javítása (java-ban nullával kezdődnek a hónapok...)

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,16 @@ public class Main {
         java.util.Calendar start = java.util.Calendar.getInstance();
         java.util.Calendar finish = java.util.Calendar.getInstance();
 
-        start.set(2017, 11, 15, 10, 15);
-        finish.set(2017, 11, 15, 12, 00);
+        start.set(2017, start.NOVEMBER, 15, 10, 15);
+        finish.set(2017, finish.NOVEMBER, 15, 12, 0);
         Event elsoOra = new Event(start.getTime(), finish.getTime(), "Elso ora", 2);
 
-        start.set(2017, 11, 15, 12, 15);
-        finish.set(2017, 11, 15, 14, 00);
+        start.set(2017, start.NOVEMBER, 15, 12, 15);
+        finish.set(2017, finish.NOVEMBER, 15, 14, 0);
         Event agilisOra = new Event(start.getTime(), finish.getTime(), "Agilis ora", 2);
 
-        start.set(2017, 11, 15, 20, 00);
-        finish.set(2017, 11, 15, 23, 00);
+        start.set(2017, start.NOVEMBER, 15, 20, 0);
+        finish.set(2017, finish.NOVEMBER, 15, 23, 0);
         Event sorozes = new Event(start.getTime(), finish.getTime(), "Sorozes", 2);
 
         Event e1 = new Event( new Date(), null, "Talalkozo Kataval", 2);
